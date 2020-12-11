d1c9a3d901d85b4b5a8ca580e57b195cbb07bf8a
Inma Gijón <igijoninf@gmail.com>
2020-12-11 12:08:04 +0100

    Added lista-correos component

---
+++ EmailAngular/src/app/components/lista-correos/lista-correos.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { defaultCipherList } from 'constants';
+
+@Component({
+  selector: 'app-lista-correos',
+  templateUrl: './lista-correos.component.html',
+  styleUrls: ['./lista-correos.component.scss']
+})
+export class ListaCorreosComponent implements OnInit {
+
+  correos: any;
+
+  constructor() { 
+    const correo1 = {
+      titulo: "Título Email 1",
+      cuerpo: `Cuerpo del email, Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,
+      Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,
+      Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email `,
+      emisor: 'correoEmisor@prueba.com',
+      receptor: 'correoReceptor@prueba.com',
+      leido: true
+    };
+    const correo2 = {
+      titulo: "Título Email 2",
+      cuerpo: `Cuerpo del email, Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,
+      Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,
+      Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email,Cuerpo del email `,
+      emisor: 'correoEmisor@prueba.com',
+      receptor: 'correoReceptor@prueba.com',
+      leido: false
+    };
+
+    this.correos = [];
+    this.correos.push(correo1);
+    this.correos.push(correo2);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
