bd18ec29d5a1ff10eea5635af9f5f42000b37487
Inma Gijón <igijoninf@gmail.com>
2020-12-20 19:52:35 +0100

    Añadido conexión y desconexión con Google utilizando ng-gapi

--- EmailAngular/src/app/app.module.ts
+++ EmailAngular/src/app/app.module.ts
@@ -9,6 +9,27 @@ import { ListaCorreosComponent } from './components/lista-correos/lista-correos.
 import { NuevoCorreoComponent } from './components/nuevo-correo/nuevo-correo.component';
 import { AvisosComponent } from './components/avisos/avisos.component';
 import { CorreosRecibidosComponent } from './views/correos-recibidos/correos-recibidos.component';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  GoogleApiModule,
+  NgGapiClientConfig,
+  NG_GAPI_CONFIG
+} from "ng-gapi";
+
+let gapiClientConfig: NgGapiClientConfig = {
+  client_id: "134377892924-a3vvhui20kl0bkefcknh7pe9ab7i5gr7.apps.googleusercontent.com",
+  discoveryDocs: ["https://analyticsreporting.googleapis.com/$discovery/rest?version=v4"],
+  ux_mode: "popup",
+  redirect_uri: "http://localhost:4200/loged",
+  scope: [
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/gmail.labels",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.readonly"
+  ].join(" ")
+};
 
 @NgModule({
   declarations: [
@@ -17,13 +38,19 @@ import { CorreosRecibidosComponent } from './views/correos-recibidos/correos-rec
     ListaCorreosComponent,
     NuevoCorreoComponent,
     AvisosComponent,
-    CorreosRecibidosComponent
+    CorreosRecibidosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    GoogleApiModule.forRoot({
+      provide: NG_GAPI_CONFIG,
+      useValue: gapiClientConfig
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
